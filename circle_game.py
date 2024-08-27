77ea92d42cdf3f0ce835008736904588885a0016
2kadrenojunkie <72770697+shadowflare420@users.noreply.github.com>
2024-08-27 15:05:19 -0400

    Update circle_game.py

--- circle_game.py
+++ circle_game.py
@@ -1,12 +1,12 @@
 from random import randint
 tries=0
 r=randint(-1000,1000)
-h=randint(-5000,5000)
-k=randint(-5000,5000)
+circlex=randint(-5000,5000)
+circley=randint(-5000,5000)
 error=0
-def move_circle();
-    h=randint(-5000,5000)
-    k=randint(-5000,5000)
+def move_circle():
+    circlex=randint(-5000,5000)
+    circley=randint(-5000,5000)
 
 while(1):
     sleep(1)
@@ -16,13 +16,13 @@ while(1):
     y=input("guess y value: ")
     if y=="": y=0
     if x=="": x=0
-    check = (int(x)-h)*2 + (int(y)-k)*2
+    check = (int(x)-circlex)*2 + (int(y)-circley)*2
     if(check<0):
         error = r*2 - (check * -1)
     else:
         error = r*2 - check
-    if(check<r*2 and check>0):
-        print("you guessed a point correctly after "+str(tries+1)+" attempt(s)! the circle had a radius of " + str(r) + " and a center of "+str(h)+","+str(k))
+    if((x - circlex)**2 + (y - circley)**2 < r**2):
+        print("you guessed a point correctly with " + str(tries+1) + " attempt(s)! the circle had a radius of " + str(r) + " and a center of "+str(circlex)+","+str(circley))
         move_circle()
         tries=0
     else:
@@ -31,10 +31,12 @@ while(1):
         if tries>200:
             print("alright dum dum, i've had enough.")
             sleep(5)
-            print("the circle had a center of " +str(h) + "," + str(k)" and a radius of " + str(r))
-
+            print("the circle had a center of " +str(circlex) + "," + str(circley)" and a radius of " + str(r))
+            move_circle()
+            tries=0
+            
         if tries>15:
             if input("give up and reveal the location? (yes/no): ")=="yes":
-                print("you gave up, cause you're a dummy! the circle had a center of " +str(h) + "," + str(k)" and a radius of " + str(r))
+                print("you gave up, cause you're a dummy! the circle ad a center of " + str(circlex) + "," + str(circley) + " and a radius of " + str(r))
                 move_circle()
                 tries=0
